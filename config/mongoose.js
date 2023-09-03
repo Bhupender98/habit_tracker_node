aeda2afc606f1f3abcb82d10d2c418bee60d597e
Bhupender98 <bhupander17440@gmail.com>
2023-09-03 15:48:51 +0530

    config updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,35 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/todo_list_db');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1/todo_list_db');
+// const db = mongoose.connection;
+// // error
+// db.on('error',console.error.bind(console,'erroe connecting to db'));
+// // up and running then message
+// db.once('open',function(){
+//     console.log('Success fully connected to the database')
+// })
+
+// mongodb + srv://bhupander17440:<password>@cluster0.ylsdpgp.mongodb.net/?retryWrites=true&w=majority
+const mongoose = require("mongoose");
+
+// storing the db on mongo atlas
+const DB = "mongodb+srv://bhupander17440:kumar12345@cluster0.ixuvsub.mongodb.net/habit?retryWrites=true&w=majority";
+
+// mongoose.connect('mongodb://127.0.0.1/habit_tracker');
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection successful!");
+    })
+    .catch((err) => console.log("no connection " + err));
+
 const db = mongoose.connection;
-// error
-db.on('error',console.error.bind(console,'erroe connecting to db'));
-// up and running then message
-db.once('open',function(){
-    console.log('Success fully connected to the database')
-})
+
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once("open", function () {
+    console.log("Connected to Database :: MongoDB");
+});
+
+module.exports = db;
+
